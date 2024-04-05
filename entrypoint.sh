426450186d4255366e9f26329a357f51d195b3f3
Dimitri JACQUEMONT <jacquemont.dim@gmail.com>
2024-04-05 11:23:34 +0200

    feat: new commit articubot, added nav

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 cd /root
-
-chmod +x robot_start.sh
-
 exec "$@"
--- robot_start.sh
+++ robot_start.sh
@@ -6,8 +6,9 @@ echo -e "\n#######################################################\n"
 echo "Please choose an option:"
 echo "1) Build project"
 echo "2) Start the robot with SLAM"
-echo "3) Start the robot without SLAM"
-echo -e "Enter your choice (1/2/3): \c"
+echo "3) Start the robot with navigation & localisation"
+echo "4) Start the robot without SLAM"
+echo -e "Enter your choice (1/2/3/4): \c"
 read choice
 
 case $choice in
@@ -26,14 +27,20 @@ case $choice in
     2)
         echo -e "\nStarting the robot with SLAM..."
         source /root/colcon_ws/install/setup.bash
-        echo -e "Configuration: SLAM enabled\n"
-        ros2 launch articubot_one launch_robot.launch.py activate_slam:=true
+        echo -e "Configuration: SLAM enabled, Navigation disabled, Localisation disabled\n"
+        ros2 launch articubot_one launch_robot.launch.py activate_slam:=true activate_nav:=false activate_loc:=false
         ;;
     3)
-        echo -e "\nStarting the robot without SLAM..."
+        echo -e "\nStarting the robot with navigation & localisation..."
         source /root/colcon_ws/install/setup.bash
-        echo -e "Configuration: SLAM disabled\n"
-        ros2 launch articubot_one launch_robot.launch.py activate_slam:=false
+        echo -e "Configuration: SLAM disabled, Navigation enabled, Localisation enabled\n"
+        ros2 launch articubot_one launch_robot.launch.py activate_slam:=false activate_nav:=true activate_loc:=true
+        ;;
+    4)
+        echo -e "\nStarting the robot naked..."
+        source /root/colcon_ws/install/setup.bash
+        echo -e "Configuration: SLAM disabled, Navigation disabled, Localisation disabled\n"
+        ros2 launch articubot_one launch_robot.launch.py activate_slam:=false activate_nav:=false activate_loc:=false
         ;;
     *)
         echo "Invalid choice. Exiting."
