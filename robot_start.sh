71572a45fd5281aed88aea10ef585512eebe6d53
Dimitri JACQUEMONT <jacquemont.dim@gmail.com>
2024-04-27 10:51:39 +0200

    feat: adding storage servo

--- robot_start.sh
+++ robot_start.sh
@@ -11,6 +11,23 @@ echo "4) Build project"
 echo -e "Enter your choice (1/2/3/4): \c"
 read choice
 
+echo -e "\nSearching for colcon_ws directory..."
+colcon_dir=$(find /home -type d -name "colcon_ws" 2>/dev/null | head -n 1)
+if [[ -z "$colcon_dir" ]]; then
+    echo "colcon_ws directory not found."
+    exit 1
+else
+    echo "colcon_ws directory found."
+fi
+
+if [ ! -f "$colcon_dir/install/setup.bash" ]; then
+    echo "File install/setup.bash not found."
+    exit 1
+else
+    source "$colcon_dir/install/setup.bash"
+    echo "File install/setup.bash sourced."
+fi
+
 case $choice in
     1)
         echo -e "\nStarting the robot naked..."
@@ -31,16 +48,9 @@ case $choice in
         ros2 launch articubot_one launch_robot.launch.py activate_slam:=false activate_nav:=true activate_loc:=true
         ;;
     4)
-        colcon_dir=$(find / -type d -name "colcon_ws" 2>/dev/null | head -n 1)
-
-        if [[ -z "$colcon_dir" ]]; then
-            echo "colcon_ws directory not found."
-            exit 1
-        else
-            echo -e "\nBuilding project..."
-            cd "$colcon_dir"
-            colcon build
-        fi
+        echo -e "\nBuilding project..."
+        cd "$colcon_dir"
+        colcon build
         ;;
     *)
         echo "Invalid choice. Exiting."
