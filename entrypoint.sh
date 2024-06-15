3cc645e05a625ce5c2339cdd0f2f2ad62ad7716b
Dimitri JACQUEMONT <jacquemont.dim@gmail.com>
2024-06-15 23:58:22 +0200

    fix: fixing docker container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp colcon_ws/src/blockbuster_core/config/bt.xml /opt/ros/humble/share/nav2_bt_navigator/behavior_trees/
+
 source /opt/ros/humble/setup.bash
 source /root/colcon_ws/install/setup.bash
 
